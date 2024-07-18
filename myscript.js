d3979d5b287f0d28f8aeb5726488dd6ab3b8f17e
Hersh <sanganihersh@gmail.com>
2024-07-18 16:16:15 -0400

    complete basic functionality with buttons

--- myscript.js
+++ myscript.js
@@ -7,7 +7,7 @@ console.log("Hello Awesome World!")
 // 1) Generates its own choice
 
 
-function getComputerChoice () {
+function getComputerChoice() {
     // Input: No parameters
     // Output: A string, one of the following: "rock", "paper", "scissors"
 
@@ -28,97 +28,108 @@ function getComputerChoice () {
 function getHumanChoice() {
 
     // To start, user enters a string exactly
-    let humanChoice = prompt("Rock, Paper, Scissors, Shoot?","");
+    let humanChoice = prompt("Rock, Paper, Scissors, Shoot?", "");
     // User is allowed to make capitalization errors but not spelling errors
     let lowerCaseAnswer = humanChoice.toLowerCase();
 
     if (lowerCaseAnswer === "rock" ||
         lowerCaseAnswer === "paper" ||
-        lowerCaseAnswer === "scissors" 
+        lowerCaseAnswer === "scissors"
     ) {
         return lowerCaseAnswer;
     } else {
-        console.log("Please type in one of the following only: \"rock\", \"paper\", or \"scissors\"!" )
+        console.log("Please type in one of the following only: \"rock\", \"paper\", or \"scissors\"!")
         return getHumanChoice();
     }
     // Later! User chooses from one of three options
-    
+
 }
 
 // 3) The choices are compared and a winner is determined
-function playRound(computerChoice, humanChoice){
-    console.log(`The computer chose ${computerChoice} and you chose ${humanChoice}!`);
+function playRound(humanChoice) {
+    computerChoice = getComputerChoice();
+    let info = `The computer chose ${computerChoice} and you chose ${humanChoice}!)`;
     let result = determineRPSWinner(computerChoice, humanChoice);
-    if (result == 1){
+    
+    if (result == 1) {
         computerScore += 1;
-        console.log("Sorry, you lose this round!");
-    } else if (result == 2){
+        info += ("/n Sorry, you lose this round!");
+    } else if (result == 2) {
         humanScore += 1;
-        console.log("You win this round!");
+        info += (" /n You win this round!");
     } else {
-        console.log("Tie, neither player wins this round!");
+        info += (" /n Tie, neither player wins this round!");
     }
-    console.log(`Score is Computer: ${computerScore} and You: ${humanScore}`);
-    //console.log("computer", computerScore)
-    //console.log("Human", humanScore)
+    
+    // Update the appropriate text boxes
+    let resultBox = document.querySelector('#results');
+    let score = document.querySelector('#score');
+
+    resultBox.textContent = info;
+    score.textContent = `Score is Computer: ${computerScore} and You: ${humanScore}`;
 }
 
-function determineRPSWinner(player1Choice, player2Choice){
+function determineRPSWinner(player1Choice, player2Choice) {
     // Returns 1 if player 1 wins, 2 if player 2 wins, and 0 for a tie
     // Assumes inputs are only "rock", "paper", or "scissors" in lower case
-    if (player1Choice == player2Choice){
+    if (player1Choice == player2Choice) {
         return 0;
-    } else if(
+    } else if (
         (player1Choice == "rock" && player2Choice == "scissors") ||
         (player1Choice == "scissors" && player2Choice == "paper") ||
         (player1Choice == "paper" && player2Choice == "rock")) {
 
-            return 1;
-        
-         } else {
-            return 2;
-         }
+        return 1;
+
+    } else {
+        return 2;
+    }
 
 }
 
-function playGame(){
-    alert("Welcome to Rock, Paper, Scissors! Are you ready?");
-
+function playGame() {
     let computerChoice;
     let humanChoice;
-    let roundNumber = 0;
-
-    // Play Best of Five: Keep playing till someone gets to 3.
-
-    while(computerScore < 3 && humanScore < 3){
-        roundNumber += 1;
-        computerChoice = getComputerChoice();
-        humanChoice = getHumanChoice();
-        console.log(`Playing Round ${roundNumber}`);
-        playRound(computerChoice, humanChoice);
-        
-        
-    }
     
-    console.log(`Played a total of ${roundNumber} rounds.`);
 
-    if (humanScore>computerScore){
+
+    if (humanScore > computerScore) {
         console.log("Congratulations! You won the game!");
-    } else{
+    } else {
         console.log("Sorry! You lost the game. Better luck next time.");
     }
 
 }
-// 4) The scores of each play are updated and displayed
-// 5) The user can play another round and start it again
-// 6) (optional) a button can be used to reset everything
+
 
 
 // Main Code
 let computerScore = 0;
 let humanScore = 0;
-playGame();
+let roundNumber = 0;
+const bestOf = 5;
+const needToWin = Math.ceil(bestOf/2);
+//playGame();
 
+let selections = document.querySelector('#buttonContainer');
+
+selections.addEventListener('click',(e)=>{
+    let target = e.target;
+    roundNumber += 1;
+    console.log(`Playing Round ${roundNumber}`);
+    playRound(target.id);
+    if( (computerScore == needToWin) || (humanScore == needToWin) ){
+        alert("Game Over!");
+    }
+    
+})
+
+
+
+
+// 4) The scores of each play are updated and displayed
+// 5) The user can play another round and start it again
+// 6) (optional) a button can be used to reset everything
 
 
 
